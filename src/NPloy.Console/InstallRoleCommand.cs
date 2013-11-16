82d6c5630fbe36355094405e4630484062e9c75d
Jasper Arildslund <jasper@arildslund.dk>
2013-11-17 00:14:26 +0100

    Creating NPloy console app to deploy NuGet packages

---
+++ src/NPloy.Console/InstallRoleCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using ManyConsole;
+
+namespace NPloy
+{
+    public class InstallRoleCommand : ConsoleCommand
+    {
+        public InstallRoleCommand()
+        {
+            IsCommand("InstallRole", "InstallRole");
+            HasAdditionalArguments(1, "Role");
+            HasOption("d|directory=", "Deploy to this directory", s => WorkingDirectory = s);
+        }
+
+        public string Role;
+        public string WorkingDirectory;
+
+        public override int Run(string[] remainingArguments)
+        {
+            var packages = new List<string>();
+
+            Role = remainingArguments[0];
+            if (Role.ToLower().EndsWith(".config") && File.Exists(Role))
+            {
+                var doc = new XmlDocument();
+                doc.Load(Role);
+                var docPackages = doc.GetElementsByTagName("package");
+                foreach (XmlNode docPackage in docPackages)
+                {
+                    packages.Add(docPackage.Attributes["id"].Value);
+                }
+
+            }
+            else
+            {
+                packages.Add(Role);
+            }
+
+            foreach (var package in packages)
+            {
+                var pProcess = new System.Diagnostics.Process
+                {
+                    StartInfo =
+                    {
+                        FileName = "nuget",
+                        Arguments = string.Format("install {0}", package),
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true
+                    }
+                };
+
+                pProcess.StartInfo.WorkingDirectory = WorkingDirectory;
+
+                pProcess.Start();
+                string strOutput = pProcess.StandardOutput.ReadToEnd();
+                Console.Write(strOutput);
+                pProcess.WaitForExit();    
+            }
+            
+            return 0;
+        }
+    }
+}
---
+++ src/NPloy.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ManyConsole;
+
+namespace NPloy
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            // locate any commands in the assembly (or use an IoC container, or whatever source)
+            var commands = GetCommands();
+
+
+            // then run them.
+            return ConsoleCommandDispatcher.DispatchCommand(commands, args, System.Console.Out);
+        }
+
+
+        public static IEnumerable<ConsoleCommand> GetCommands()
+        {
+            return ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof (Program));
+        }
+    }
+}
