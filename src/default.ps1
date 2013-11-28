1a7d257fabb813bc58f8d3db36ed7cfcd3e222cd
Jasper Arildslund <jasper@arildslund.dk>
2013-11-28 22:55:03 +0100

    Added NuSpec file

--- src/default.ps1
+++ src/default.ps1
@@ -7,3 +7,7 @@ Task Build {
 Task Test -depends Build {
    Exec { packages\NUnit.Runners.2.6.3\tools\nunit-console.exe NPloy.Console.UnitTests\bin\debug\NPloy.Console.UnitTests.dll /xml=TestResult.xml  }   
 }
+
+Task Pack -depends Test {
+   Exec { nuget pack NPloy.Console\NPloy.Console.nuspec -OutputDirectory d:\temp\packages }  
+}
