16a5c44e9e467cdd2895c3ed9af479d32672dd63
Jasper Arildslund <jasper@arildslund.dk>
2013-12-02 22:46:28 +0100

    Created nuget package for web

---
+++ samples/NPloy.Samples.Web/App_Install/Install.ps1
@@ -0,0 +1,33 @@
+﻿Param($PackagePath)
+
+$ErrorActionPreference = "stop"
+
+try
+{	
+	Import-Module ApplicationServer
+	Import-Module WebAdministration
+	
+	#[Reflection.Assembly]::LoadWithPartialName("System.Messaging")
+
+
+	$appName = "NPloySample"
+	$appPool = "DefaultAppPool"
+	$appPath = $PackagePath
+	$site = Get-WebSite | where { $_.Name -eq $appName }
+	if($site -eq $null)
+	{
+	#	New-WebApplication -Name $appName -ApplicationPool $appPool -PhysicalPath $appPath -Site "Default Web Site" -force
+	#	Invoke-Expression -command "c:\windows\system32\inetsrv\appcmd.exe set app 'Default Web Site/$appName' /enabledprotocols:'$protocols'"
+	}
+	else
+	{
+	#	WriteToHost "Web application $appName already exists"
+	}
+}
+catch
+{
+    $ErrorActionPreference = "Continue";   
+    Write-Error $_
+
+    Exit 1
+}
--- samples/default.ps1
+++ samples/default.ps1
@@ -8,5 +8,6 @@ Task Pack -depends Build {
    Exec { & nuget pack NPloy.Samples.WindowsService\NPloy.Samples.WindowsService.nuspec -NoPackageAnalysis -OutputDirectory ..\packages }
    Exec { & nuget pack NPloy.Samples.SmokeTests\NPloy.Samples.SmokeTests.nuspec -NoPackageAnalysis -OutputDirectory ..\packages }
    Exec { & nuget pack NPloy.Samples.Iis\NPloy.Samples.Iis.nuspec -NoPackageAnalysis -OutputDirectory ..\packages }
+   Exec { & nuget pack NPloy.Samples.Web\NPloy.Samples.Web.nuspec -NoPackageAnalysis -OutputDirectory ..\packages }
    Exec { & xcopy /y /e .nploy\*.* ..\install\.nploy\}
 }
