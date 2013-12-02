a7f54a31edb305bfed72f07b9c1e71d4095a5166
Jasper Arildslund <jasper@arildslund.dk>
2013-12-02 22:12:57 +0100

    Adding infrastructure package

---
+++ samples/NPloy.Samples.Iis/App_Install/Install.ps1
@@ -0,0 +1,18 @@
+﻿Param($ServiceUsername, $ServicePassword)
+
+$ErrorActionPreference = "stop"
+
+try
+{	
+	Import-Module servermanager
+	Add-WindowsFeature Web-Server
+	Add-WindowsFeature Application-Server
+	Add-WindowsFeature MSMQ
+}
+catch
+{
+    $ErrorActionPreference = "Continue";   
+    Write-Error $_
+
+    Exit 1
+}
